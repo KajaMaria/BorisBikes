90de9305e318d260b7e3162fdaeb936587904df4
Rianne McCartney <riannemccartney@hotmail.com>
2019-06-26 16:52:53 +0100

    exercise 12 - error message added

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,16 +3,24 @@ require 'DockingStation'
 describe DockingStation do
   it { should respond_to(:release_bike) }
 
-  it 'release working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  describe '#release_bike' do
+    it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
 
-it 'returns docked bike' do
-  bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bike).to eq bike
-end
+      it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+    end
+
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
   # it 'docks bikes' do
   #   bike = subject.docking_bike
